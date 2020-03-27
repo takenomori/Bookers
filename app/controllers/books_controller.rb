69f612e1769e566a97ee51a0e0782ef037d0a207
takeru0922 <moriai0922takeru@gmail.com>
2020-03-28 03:28:38 +0900

    save

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,6 @@
-class BookssController < ApplicationController
+class BooksController < ApplicationController
   def index
     @books = Book.all
-  end
-
-  def new
     @book = Book.new
   end
 
@@ -27,10 +24,10 @@ class BookssController < ApplicationController
     redirect_to book_path(book)
   end
 
-  def destroy	
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+  def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to books_path
   end
 
   private
@@ -38,4 +35,5 @@ class BookssController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
