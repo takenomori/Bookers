eee5d54dac1984a1e57afcb24bc0f5d0717c7d8b
takeru0922 <moriai0922takeru@gmail.com>
2020-03-29 19:43:27 +0900

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,17 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "successfully"
+    redirect_to book_path(@book.id)
+    else 
+    @books = Book.all
+      render action: :index
+    end
+  end
+
   def show
     @book = Book.find(params[:id])
   end
@@ -12,15 +23,11 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
-
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "successfully"
+    
     redirect_to book_path(book)
   end
 
